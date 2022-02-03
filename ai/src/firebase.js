7dea7d4035d0930fe294ea3cd7c1ac73382d1d11
Anmol <anmolsoni2704@gmail.com>
2022-02-03 23:31:56 +0530

    first commit

---
+++ ai/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA-YSHUK89tlhSJSNgkXJ1WEMmJoKeOow8",
+    authDomain: "alan-ai-news-app-8d066.firebaseapp.com",
+    projectId: "alan-ai-news-app-8d066",
+    storageBucket: "alan-ai-news-app-8d066.appspot.com",
+    messagingSenderId: "1064611023041",
+    appId: "1:1064611023041:web:66bf48e4e78c6902c72c40"
+  };
